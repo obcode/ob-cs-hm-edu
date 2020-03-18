2149c720fa0ed13cdbda80369d8e0be75ef3d555
Oliver Braun <ob@cs.hm.edu>
2020-03-18 10:17:58 +0100

    use time in date...

--- src/misc/dateHelper.js
+++ src/misc/dateHelper.js
@@ -1,24 +1,26 @@
-function makeDate(str) {
+function makeDate(str, daytime) {
   // assume str == "07.10.2019"
-  return new Date(str.replace(/(\d{2})\.(\d{2})\.(\d{4})/, "$3-$2-$1"));
-}
-
-function today() {
-  let d = new Date();
-  d.setHours(0, 0, 0, 0);
-  return d;
+  // assume daytime = "dienstags, 11:45 - 13:15"
+  const time = daytime
+    .split("-")[1]
+    .trim()
+    .split(":");
+  const date = new Date(str.replace(/(\d{2})\.(\d{2})\.(\d{4})/, "$3-$2-$1"));
+  date.setHours(time[0], time[1]);
+  console.log(date);
+  return date;
 }
 
 function nextLecture(lecture) {
   if (!lecture.lectures) return undefined;
-  const t = today();
-  return lecture.lectures.find(l => makeDate(l.date) >= t);
+  const t = new Date();
+  return lecture.lectures.find(l => makeDate(l.date, lecture.daytime) >= t);
 }
 
 function nextLabs(lab) {
   if (!lab.labs) return undefined;
   let labsPerGroup = [];
-  const t = today();
+  const t = new Date();
   for (const i in lab.labs[1].dates) {
     labsPerGroup[i] = lab.labs
       .map(l => {
@@ -28,7 +30,7 @@ function nextLabs(lab) {
           cancelled: l.cancelled || l.dates[i].cancelled
         };
       })
-      .find(l => makeDate(l.date) >= t);
+      .find(l => makeDate(l.date, lab.groups[i].daytime) >= t);
   }
   return labsPerGroup;
 }
